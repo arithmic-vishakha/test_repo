eab30ea075c7d51bd98811c997abebc5fe3c1199
arithmic-vishakha <vishakha@foliumlabs.net>
2025-07-29 15:45:55 +0530

    Create test_file_3.py

---
+++ test_file_3.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+# Input from the user
+num = 4
+
+# Check and print result
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
